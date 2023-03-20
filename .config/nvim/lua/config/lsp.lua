42c456f2788c8dbb5f26911487881027bfdf3ca8
Andras Banki-Horvath <bhandras@gmail.com>
2023-03-20 22:19:23 +0100

    fix lua ls

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -32,33 +32,26 @@ local servers = {
             typeCheckingMode = "off",
         },
     },
-    sumneko_lua = {
+    lua_ls = {
         settings = {
             Lua = {
                 runtime = {
                     -- Tell the language server which version of Lua you're
                     -- using (most likely LuaJIT in the case of Neovim).
                     version = "LuaJIT",
-                    -- Setup your lua path
-                    path = vim.split(package.path, ";"),
                 },
                 diagnostics = {
                     -- Get the language server to recognize the `vim` global
                     globals = {
-                        "vim", "describe", "it", "before_each", "after_each",
-                        "packer_plugins",
+                        "vim",
                     },
                 },
                 workspace = {
                     -- Make the server aware of Neovim runtime files
-                    library = {
-                        [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-                        [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
-                    },
+                    library = vim.api.nvim_get_runtime_file("", true),
                     maxPreload = 2000,
                     preloadFileSize = 50000,
                 },
-                completion = { callSnippet = "Both" },
                 telemetry = { enable = false },
                 hint = {
                     enable = true,
